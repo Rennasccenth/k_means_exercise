aa7e7d3650dca556478bd0a6695ec8eef52c96fc
Felipe Nunes <rennasccenth@gmail.com>
2022-03-14 00:42:03 -0300

    feat: Undo overkill

--- src/services/interfaces/model_service.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ModelService(ABC):
-    """Service than provides pre-configured neural network methods."""
-
-    @abstractmethod
-    async def get_kmeans_model(self):
-        """Returns the k-means implementation."""
